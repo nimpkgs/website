fa1e181f4c351487be179619937f05ed9babd0ed
Daylin Morgan <daylinmorgan@gmail.com>
2023-11-25 02:20:34 -0600

    feat: add informative note

--- src/pages/index.nim
+++ src/pages/index.nim
@@ -6,10 +6,11 @@ import ../context
 proc render*(): VNode =
   result = buildHtml(tdiv(class = "justify-center")):
     tdiv(class = "flex flex-col space-y-5"):
+      tdiv(class = "md:text-4xl text-2xl font-bold font-mono-casual text-center"):
+        text "discover Nim's ecosystem of third-party libraries and tools"
       tdiv(class = "grow md:w-4/5 mx-auto"):
         tdiv(class = "flex flex-col md:flex-row grow"):
           searchBar()
-          # ctx.randomPackage()
       tdiv():
         tdiv():
           text "explore tags:"
